c1ecfa2d9f0111453545736ec5c0d9ea7ccb20dd
albertofragoso <hola@albertofragoso.com>
2019-07-13 11:29:03 -0500

    Creating Login component

---
+++ src/pages/Login.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Login = () => (
+  <div className="Login">
+    <div className="Login-container">
+      <div className="Login-content">
+        <h2>Sign up or Log in</h2>
+        <button>
+          <i>FB</i>
+          <span>Log in with Facebook</span>
+        </button>
+      </div>
+    </div>
+  </div>
+)
+
+export default Login
