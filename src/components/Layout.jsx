667135fc81c40056aa3156421d1a6f535d64c107
albertofragoso <hola@albertofragoso.com>
2019-07-13 10:56:15 -0500

    Adding Layout to main App

---
+++ src/components/Layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
+import Home from '../pages/Home'
+
+const Layout = props => (
+  <div className="Layout">
+    <Header />
+    <Home />
+    <Footer />
+  </div>
+)
+
+export default Layout
