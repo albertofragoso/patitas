c0389dd04b735fcd198089ef6690378924ca4ee6
albertofragoso <hola@albertofragoso.com>
2019-07-18 22:34:11 -0500

    Creating upload files and storage to Firebase

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,34 +1,51 @@
 import React from 'react'
+import { storage, database } from '../utils/firebase'
 
-const Form = () => (
-  <div className="Form">
-    <div className="Form-head">
-      <h2>Put up for adoption</h2>
+const Form = () => {
+
+  const handleUpload = e => {
+    const file = e.target.files[0]
+    const storageRef = storage.ref()
+    const name = (+new Date()) + '-' + file.name    
+    const uploadFile = storageRef.child(name).put(file)
+ 
+    uploadFile
+      .then(snapshot => {
+        snapshot.ref.getDownloadURL()
+          .then(downloadURL => console.log(downloadURL))
+      })
+  }
+
+  return (
+    <div className="Form">
+      <div className="Form-head">
+        <h2>Put up for adoption</h2>
+      </div>
+      <div className="Form-item">
+        <form>
+          <input type="text" name="name" placeholder="Pet's name..." />
+          <input type="text" name="description" placeholder="Describe your pet..." />
+          <select name="type">
+            <option disabled  selected>Choose their name...</option>
+            <option value="dog">🐶</option>
+            <option value="cat">😼</option>
+          </select>
+          <select name="gender">
+            <option disabled selected>Chosee their gender...</option>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+          </select>
+          <select name="adopt">
+            <option disabled selected></option>
+            <option value="true">Put up for adoption</option>
+            <option value="false">Take care</option>
+          </select>
+          <input type="file" name="photo" onChange={handleUpload} />
+          <button>Let's go 🚀</button>
+        </form>
+      </div>
     </div>
-    <div className="Form-item">
-      <form>
-        <input type="text" name="name" placeholder="Pet's name..." />
-        <input type="text" name="description" placeholder="Describe your pet..." />
-        <select name="type">
-          <option disabled  selected>Choose their name...</option>
-          <option value="dog">🐶</option>
-          <option value="cat">😼</option>
-        </select>
-        <select name="gender">
-          <option disabled selected>Chosee their gender...</option>
-          <option value="male">Male</option>
-          <option value="female">Female</option>
-        </select>
-        <select name="adopt">
-          <option disabled selected></option>
-          <option value="true">Put up for adoption</option>
-          <option value="false">Take care</option>
-        </select>
-        <input type="file" name="photo" />
-        <button>Let's go 🚀</button>
-      </form>
-    </div>
-  </div>
-)
+  )
+}
 
 export default Form
