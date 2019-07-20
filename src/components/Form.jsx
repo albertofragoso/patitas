be08e73b4c1d1f8554dd825350755b3d83234c91
albertofragoso <hola@albertofragoso.com>
2019-07-20 13:08:07 -0500

    Handling form data and pushing to Firebase database

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,6 +3,29 @@ import { storage, database } from '../utils/firebase'
 
 const Form = () => {
 
+  const handleSubmit = e => {
+    e.preventDefault()
+    const form = new FormData(e.target)
+    const newDate = new Date().toISOString()
+
+    const data = {
+      'name': form.get('name'),
+      'description': form.get('description'),
+      'type': form.get('type'),
+      'gender': form.get('gender'),
+      'adopt': form.get('adopt'),
+      'date': newDate,
+      'photo': '',
+      'profilePic': '',
+      'userContact': '',
+      'userName': ''
+    }
+
+    database.ref('pets').push(data)
+      .then(response => console.log(response))
+      .catch(error => console.log(error))
+  }
+
   const handleUpload = e => {
     const file = e.target.files[0]
     const storageRef = storage.ref()
@@ -22,7 +45,7 @@ const Form = () => {
         <h2>Put up for adoption</h2>
       </div>
       <div className="Form-item">
-        <form>
+        <form onSubmit={handleSubmit}>
           <input type="text" name="name" placeholder="Pet's name..." />
           <input type="text" name="description" placeholder="Describe your pet..." />
           <select name="type">
